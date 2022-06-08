facd561bc14a95ef1c3d76bf758f5d0d07876af1
liam <liam.missin@gmail.com>
2022-06-08 10:50:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# BSV Example
+
+To run
+
+`npm install`
+
+`node index.js generate`
+
+This generates a private key and prints out the WIF (Wallet Interchange Format) and address.
+The WIF will be used later. Now send some test coins to your address using the faucet website https://faucet.bitcoincloud.net
+
+Now call write adding your WIF as another arguement.
+`node index.js write KwpVuu478dqjR7R7qUnUMqdC72X2X5HrHy39djdTMsDSwCVTK17B`
+
+This will return the transaction Id which you can use to look up the transaction on [test.whatsonchain.com](https://test.whatsonchain.com/)
---
+++ index.js
@@ -0,0 +1,97 @@
+const bsv = require('bsv')
+const axios = require('axios')
+
+const myArgs = process.argv.slice(2)
+
+switch (myArgs[0]) {
+  case 'generate':
+    generateAddress()
+    break
+  case 'write':
+    if (!myArgs[1]) {
+      console.log('please supply your WIF.')
+      break
+    }
+    write(myArgs[1])
+    break
+  default:
+    console.log('Usage:\ngenerate: create a wallet\nwrite <WIF>: write data to the chain')
+}
+
+// generate testnet address
+
+// print the WIF and return
+function generateAddress () {
+  const privateKey = bsv.PrivateKey()
+  const wif = privateKey.toWIF()
+  const addr = privateKey.toAddress('testnet').toString()
+  console.log('Your WIF:     ' + wif)
+  console.log('Your address: ' + addr)
+  console.log('Send some test coins to the address from to https://faucet.bitcoincloud.net/')
+}
+
+async function write (wif) {
+  const privateKey = bsv.PrivateKey.fromWIF(wif)
+  const publicKeyHash = bsv.crypto.Hash.sha256ripemd160(privateKey.publicKey.toBuffer()).toString('hex')
+
+  const addr = privateKey.toAddress('testnet').toString()
+
+  // get UTXOs
+  // GET https://api.whatsonchain.com/v1/bsv/<network>/address/<address>/unspent
+  const response = await axios.get(`https://api.whatsonchain.com/v1/bsv/test/address/${addr}/unspent`)
+
+  const unspentArr = response.data
+
+  // unspent is an array eg,
+  // [
+  //    {
+  //        height: 0,
+  //        tx_pos: 0,
+  //        tx_hash: '7aec649a14d752e86bcb419324e88c73433bf8043b6aa16b7fb38b3a90ec4c27',
+  //        value: 99300
+  //    }
+  // ]
+
+  if (unspentArr.length < 1) {
+    console.log('no UTXOs for address ' + addr)
+    console.log('get some test coins from to https://faucet.bitcoincloud.net/')
+  }
+  const unspent = unspentArr[0]
+
+  // create a utxo object
+  const utxo = {
+    txid: unspent.tx_hash,
+    outputIndex: unspent.tx_pos,
+    scriptPubKey: bsv.Script.fromASM(`OP_DUP OP_HASH160 ${publicKeyHash} OP_EQUALVERIFY OP_CHECKSIG`).toHex(),
+    amount: unspent.value / 1E8
+  }
+
+  const tx = new bsv.Transaction()
+  tx.from(utxo)
+
+  // add data output
+  tx.addSafeData('test')
+
+  // add change
+  tx.addOutput(new bsv.Transaction.Output({
+    script: bsv.Script.fromASM(`OP_DUP OP_HASH160 ${publicKeyHash} OP_EQUALVERIFY OP_CHECKSIG`).toHex(),
+    satoshis: unspent.value - 50
+  }))
+  tx.sign(privateKey)
+
+  // submit tx
+  const txHex = tx.toString('hex')
+
+  const url = 'https://api.whatsonchain.com/v1/bsv/test/tx/raw'
+
+  const submitResponse = await axios({
+    method: 'post',
+    url: url,
+    data: {
+      txhex: txHex
+    }
+  })
+
+  const txid = submitResponse.data
+  console.log('txid: ' + txid)
+}
